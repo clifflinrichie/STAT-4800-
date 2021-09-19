2db15687810a7f32f488fd204923d5f3d7108e74
JoshuaJeon <jj5hp@virginia.edu>
2021-09-19 16:26:10 -0400

    Here lies hw2 and all my sorrows
    this sux

---
+++ HW2.py
@@ -0,0 +1,9 @@
+## HW2 | 9/23 | Andy Chuang, Josh Jeon, Cliff Lin
+"""
+A good "metaphor" for using Markov chains in the ranking problem could be a "bandwagon fan," a fan who tends to support
+whichever team is doing better. Each team starts with the same amount of fans - each fan has the choice to change teams or
+stay with their current team. They can choose after each iteration.
+"""
+
+import pandas as pd
+import numpy as np
