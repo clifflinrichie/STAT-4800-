7f981b7185e13a98924b73da1689e9f9f1f29c7c
JoshuaJeon <jj5hp@virginia.edu>
2021-11-03 14:54:44 -0400

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -32,23 +32,42 @@ for value in filtered_data['Seconds_Elapsed']:
 filtered_data5['time'] = newcol2
 
 # Groupings
-# period_groups = dict(filtered_data2['Period'].value_counts())
-# strength_groups = dict(filtered_data2['Strength'].value_counts())
-# time_groups = dict(filtered_data2['time'].value_counts())
-# id_groups = dict(filtered_data2['Game_Id'].value_counts())
-# team_groups = dict(filtered_data2['Ev_Team'].value_counts())
 u_period = filtered_data5['Period'].unique()
-u_strength = ['5x5','5x4','4x5']
+u_strength = ['5x5','5x4','4x5'] 
+u_strength1 = [1,2,3] # three levels: 5x5, 5x4, 4x5 repsectfully
 u_time = filtered_data5['time'].unique()
 counts = []
 x = []
+
 for p in u_period:
     for s in u_strength:
         for t in u_time:
             counts.append(len(filtered_data5[(filtered_data5['Period']==p)&(filtered_data5['Strength']==s)&(filtered_data5['time']==t)]))
+for p in u_period:
+    for s in u_strength1:
+        for t in u_time:
             x.append([p,s,t])
+
+for i,v in enumerate(x):
+    v.append(counts[i])
+final_array = np.array(x)
+final_df = pd.DataFrame(final_array, columns = ['Period','Strength','Time Bin','Shot Rate'])
 
 # Model Building
 from sklearn.linear_model import PoissonRegressor
+from sklearn.model_selection import train_test_split
+train, test = train_test_split(final_df, test_size=0.2)
+y_train = np.array(train['Shot Rate'].copy())
+train.drop(columns='Shot Rate', axis=1, inplace=True)
+x_train = np.array(train.copy())
+
+y_test = np.array(test['Shot Rate'])
+test.drop(columns='Shot Rate', axis=1, inplace=True)
+x_test = np.array(test.copy())
+
 pr = PoissonRegressor()
+pr.fit(x_train,y_train)
+print(pr.score(x_train,y_train))
+y_pred = pr.predict(x_test)
+print(pr.coef_)
 
